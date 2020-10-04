41d57b8d3f67d7c2ee747620f6c7c4b41eb595c9
Sangavi Loganathan <48724073+Sangavi-123@users.noreply.github.com>
2020-10-04 08:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ label1='IBU'
 label2='ABV'
 githublink='https://github.com/austinlasseter/flying-dog-beers'
 sourceurl='https://www.flyingdog.com/beers/'
-
+data= pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv")
 
 fig = go.Figure(data=go.Scatter(x=data['Postal'],
                                 y=data['Population'],
