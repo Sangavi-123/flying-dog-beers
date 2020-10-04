723a7b484ccb70a9d963a362c76b31c984216ac9
Sangavi Loganathan <48724073+Sangavi-123@users.noreply.github.com>
2020-10-04 18:49:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,40 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
+mytitle='Sample Comparison'
+tabtitle='sample!'
+myheading='Sample check'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
 
-data= pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv")
+########### Set up the chart
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name=label1,
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name=label2,
+    marker={'color':color2}
+)
 
-fig = go.Figure(data=go.Scatter(x=data['Postal'],
-                                y=data['Population'],
-                                mode='markers',
-                                marker_color=data['Population'],
-                                text=data['State'])) # hover text goes here
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = mytitle
+)
 
-fig.update_layout(title='Population of USA States')
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
 ########### Initiate the app
@@ -24,10 +48,14 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
+    html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
-        figure=fig
+        figure=beer_fig
     ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A('Data Source', href=sourceurl),
     ]
 )
 
