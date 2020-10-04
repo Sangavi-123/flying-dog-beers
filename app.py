396a0820231d267d9d7bbd785e7b642236c8cdf7
Sangavi Loganathan <48724073+Sangavi-123@users.noreply.github.com>
2020-10-04 08:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Sample Comparison'
-tabtitle='sample!'
-myheading='Sample check'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+
 data= pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv")
 
 fig = go.Figure(data=go.Scatter(x=data['Postal'],
@@ -35,14 +24,10 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
         figure=fig
     ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
     ]
 )
 
