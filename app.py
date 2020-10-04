0826ce46926cd5e8538d65d1373ae829ad86011b
Sangavi Loganathan <48724073+Sangavi-123@users.noreply.github.com>
2020-10-04 19:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,12 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Sample Comparison'
-tabtitle='sample!'
-myheading='Sample check'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+url ='https://github.com/Sangavi-123/Dash_App_Cluster/raw/main/Dell_Eng_Talent_byLocations.xlsx'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+df = pd.read_excel(url)
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+fig = px.scatter(df, x = 'Normalized Value', y = column, color="Clusters", hover_name = 'locations')
+        figure.update_traces(marker=dict(size=15))
 
 
 ########### Initiate the app
@@ -51,7 +23,7 @@ app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
-        figure=beer_fig
+        figure=fig
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
